656c707dfc619189f6ac2d8f65e33cf34a5af7e6
yannay399 <yannay399@gmail.com>
2025-07-25 23:04:11 +0200

    composser create project y migraciones

--- src/public/index.php
+++ src/public/index.php
@@ -1,39 +1,20 @@
 <?php
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Detectar entorno local o producción según existencia de carpeta o archivo
-if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    // Estamos en entorno local (estructura estándar Laravel)
-    $basePath = realpath(__DIR__ . '/../');
-    //echo "Ejecución en local<br>";
-} elseif (file_exists(__DIR__ . '/src/vendor/autoload.php')) {
-    // Estamos en producción en InfinityFree con estructura modificada
-    $basePath = realpath(__DIR__ . '/src/');
-    echo "Ejecución en producción <br>";
-} else {
-    die("No se ha podido detectar el entorno de ejecución. Path:" . realpath(__DIR__ . '/src/'));
-}
-
-// Modo mantenimiento
-if (file_exists($maintenance = $basePath . 'storage/framework/maintenance.php')) {
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Autoloader de Composer
-require $basePath . '/vendor/autoload.php';
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel y manejo de la petición
+// Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once $basePath . '/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-// Capturar la petición HTTP y manejarla
-$response = $app->handle(
-    $request = Request::capture()
-);
-
-$response->send();
-
-$app->terminate($request, $response);
+$app->handleRequest(Request::capture());
